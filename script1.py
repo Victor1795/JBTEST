b6dfdb63812de0bf73c403bd9dd4064f9610912b
Victor1795 <vic.abdiel@gmail.com>
2021-01-22 18:16:28 +0000

    update2

--- script1.py
+++ script1.py
@@ -8,7 +8,7 @@ import csv
 data = pd.read_csv("TODOS.csv")
 
 # selection
-df = pd.DataFrame(data, columns= ['Client','Project','Role Title','Role ID','IQN','Status','POC','Resource Start Date','Role Created Date','Resource End Date'])
+df = pd.DataFrame(data, columns= ['Summary','Issue key','Issue id','Parent id','Issue Type','Status','Custom field (POC JBT)','Custom field (Role Title)','Custom field (IQN)','Custom field (Client JBT)','Custom field (Project JBT)','Custom field (Role Title)','Custom field (Role Id)','Custom field (Resource Start Date)','Custom field (Role Created)','Custom field (Resource End Date)'])
 
 # create unique value field "Issue Type"
 #df["Issue Type"] = 'Demand'
