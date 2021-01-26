46e300c43dc91d23fcd175624dcaddbfef6db081
Victor Diaz de Leon <vic.abdiel@gmail.com>
2021-01-26 12:57:24 -0600

    Delete script2.py

--- script2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import pandas as pd
-import csv
-
-################################# Reading csv file & selecting important fields #####################################
-data = pd.read_csv("global_data.csv")
-
-print(data)
